0e73de2d994caaedc060168830004b087f413aac
cr3pt <cr3pt@o2.pl>
2014-05-27 19:48:18 +0200

    add full_d605.ml for correct AOSPA build

---
+++ full_d605.mk
@@ -0,0 +1,47 @@
+#
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit device configuration
+$(call inherit-product, device/lge/d605/d605.mk)
+
+# trebuchet
+PRODUCT_PACKAGES += Trebuchet
+
+# Release name
+PRODUCT_RELEASE_NAME := OptimusL9II
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := d605
+PRODUCT_NAME := cm_d605
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-D605
+PRODUCT_MANUFACTURER := lge
